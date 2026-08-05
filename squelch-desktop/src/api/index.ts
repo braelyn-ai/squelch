41aeceb79a391af0bfae72502be9837781d6087f
Braelyn Boynton <bboynton97@gmail.com>
2026-08-04 19:55:15 -0700

    retire squelch-desktop: the client is Passband now

--- squelch-desktop/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * as api from "./client";
-export * from "./types";
-export { ApiError } from "./errors";
-export type { ApiErrorKind } from "./errors";
-export { getSettings, setSettings, clearSettings } from "./settings";
-export type { Settings } from "./settings";
--- squelch-desktop/src/keys/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-  useKeys,
-  useKeyContext,
-  registerKeys,
-  pushContext,
-  currentContext,
-  allBindings,
-} from "./useKeys";
-export type { KeyBinding, KeyContext } from "./useKeys";
